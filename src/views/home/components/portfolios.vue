44ecacacc740a4a79f04d39e807c7203588baf01
guowei <842737820@qq.com>
2023-09-11 16:29:38 +0800

    new

--- src/views/home/components/portfolios.vue
+++ src/views/home/components/portfolios.vue
@@ -1,14 +1,192 @@
 <template>
-  <div class='container'>
-    prot
+  <div class='portfolio-con'>
+    <div class="con-title">Portfolios Projects</div>
+    <div class="con-title1">We have successfully incubated and invested in leading projects in its early stages. With
+      comprehensive knowledge of frontier technology and zero-to-one project experience, we can add greater value to our
+      portfolio projects in addition to financial investment.</div>
+    <div class="con-list">
+      <div class="con-list-title">
+        <div>Portfolio Funding</div>
+        <div class="con-title-line"></div>
+      </div>
+      <div class="portfolio-list">
+        <div class="portfolio-li">
+          <div class="portfolio-li-imgs">
+            <img class="portfolio-li-img" src="../../../assets/images/funding-1.png" alt="" srcset="">
+          </div>
+          <div class="portfolio-li-con">
+            <div class="p-li-title">Nomad Capital</div>
+            <div class="p-li-txt">Description text instance, up to two lines</div>
+            <div class="p-li-con">
+              <img src="../../../assets/images/svg/svg1.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/svg2.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/svg3.svg" alt="" srcset="">
+            </div>
+          </div>
+        </div>
+        <div class="portfolio-li">
+          <div class="portfolio-li-imgs">
+            <img class="portfolio-li-img" src="../../../assets/images/funding-2.png" alt="" srcset="">
+          </div>
+          <div class="portfolio-li-con">
+            <div class="p-li-title">GEEKCARTEL</div>
+            <div class="p-li-txt">Description text instance, up to two lines</div>
+            <div class="p-li-con">
+              <img src="../../../assets/images/svg/svg1.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/svg2.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/svg3.svg" alt="" srcset="">
+            </div>
+          </div>
+        </div>
+        <div class="portfolio-li">
+          <div class="portfolio-li-imgs">
+            <img class="portfolio-li-img" src="../../../assets/images/funding-3.png" alt="" srcset="">
+          </div>
+          <div class="portfolio-li-con">
+            <div class="p-li-title">MetaWeb Ventures</div>
+            <div class="p-li-txt">Description text instance, up to two lines</div>
+            <div class="p-li-con">
+              <img src="../../../assets/images/svg/svg1.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/svg2.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/svg3.svg" alt="" srcset="">
+            </div>
+          </div>
+        </div>
+        <div class="portfolio-li">
+          <div class="portfolio-li-imgs">
+            <img class="portfolio-li-img" src="../../../assets/images/funding-4.png" alt="" srcset="">
+          </div>
+          <div class="portfolio-li-con">
+            <div class="p-li-title">UpHonest Capital</div>
+            <div class="p-li-txt">Description text instance, up to two lines</div>
+            <div class="p-li-con">
+              <img src="../../../assets/images/svg/svg1.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/svg2.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/svg3.svg" alt="" srcset="">
+            </div>
+          </div>
+        </div>
+        <div class="portfolio-li">
+          <div class="portfolio-li-imgs">
+            <img class="portfolio-li-img" src="../../../assets/images/funding-5.png" alt="" srcset="">
+          </div>
+          <div class="portfolio-li-con">
+            <div class="p-li-title">Draper Dragon</div>
+            <div class="p-li-txt">Description text instance, up to two lines</div>
+            <div class="p-li-con">
+              <img src="../../../assets/images/svg/svg1.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/svg2.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/svg3.svg" alt="" srcset="">
+            </div>
+          </div>
+        </div>
+        <div class="portfolio-li">
+          <div class="portfolio-li-imgs">
+            <img class="portfolio-li-img" src="../../../assets/images/funding-6.png" alt="" srcset="">
+          </div>
+          <div class="portfolio-li-con">
+            <div class="p-li-title">ShangBay Capital</div>
+            <div class="p-li-txt">Description text instance, up to two lines</div>
+            <div class="p-li-con">
+              <img src="../../../assets/images/svg/svg1.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/svg2.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/svg3.svg" alt="" srcset="">
+            </div>
+          </div>
+        </div>
+        <div class="portfolio-li">
+          <div class="portfolio-li-imgs">
+            <img class="portfolio-li-img" src="../../../assets/images/funding-7.png" alt="" srcset="">
+          </div>
+          <div class="portfolio-li-con">
+            <div class="p-li-title">IOSG Venture</div>
+            <div class="p-li-txt">Description text instance, up to two lines</div>
+            <div class="p-li-con">
+              <img src="../../../assets/images/svg/svg1.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/svg2.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/svg3.svg" alt="" srcset="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="con-listt">
+      <div class="con-list-title">
+        <div>Portfolio-Projects</div>
+        <div class="con-title-line"></div>
+      </div>
+      <div class="portfolio-listt">
+        <div class="portfolio-li">
+          <div class="portfolio-li-imgst">
+            <img class="portfolio-li-imgt" src="../../../assets/images/projects-1.png" alt="" srcset="">
+          </div>
+          <div class="portfolio-li-cont">
+            <img class="portfolio-li-cont-img" src="../../../assets/images/projects-hover-1.png" alt="" srcset="">
+            <div class="p-li-title">Nomad Capital</div>
+            <div class="p-li-txt">Description text instance, up to two lines</div>
+            <div class="p-li-con">
+              <img src="../../../assets/images/svg/dark-svg1.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/dark-svg2.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/dark-svg3.svg" alt="" srcset="">
+            </div>
+          </div>
+        </div>
+        <div class="portfolio-li">
+          <div class="portfolio-li-imgst" style="background-color: #EEE;">
+            <img class="portfolio-li-imgt" src="../../../assets/images/projects-2.png" alt="" srcset="">
+          </div>
+          <div class="portfolio-li-cont">
+            <img class="portfolio-li-cont-img" src="../../../assets/images/projects-hover-1.png" alt="" srcset="">
+            <div class="p-li-title">Nomad Capital</div>
+            <div class="p-li-txt">Description text instance, up to two lines</div>
+            <div class="p-li-con">
+              <img src="../../../assets/images/svg/dark-svg1.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/dark-svg2.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/dark-svg3.svg" alt="" srcset="">
+            </div>
+          </div>
+        </div>
+        <div class="portfolio-li">
+          <div class="portfolio-li-imgst" style="background-color: #FFF;">
+            <img class="portfolio-li-imgt" src="../../../assets/images/projects-1.png" alt="" srcset="">
+          </div>
+          <div class="portfolio-li-cont">
+            <img class="portfolio-li-cont-img" src="../../../assets/images/projects-hover-1.png" alt="" srcset="">
+            <div class="p-li-title">Nomad Capital</div>
+            <div class="p-li-txt">Description text instance, up to two lines</div>
+            <div class="p-li-con">
+              <img src="../../../assets/images/svg/dark-svg1.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/dark-svg2.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/dark-svg3.svg" alt="" srcset="">
+            </div>
+          </div>
+        </div>
+        <div class="portfolio-li">
+          <div class="portfolio-li-imgst" style="background-color: #EEE;">
+            <img class="portfolio-li-imgt" src="../../../assets/images/projects-1.png" alt="" srcset="">
+          </div>
+          <div class="portfolio-li-cont">
+            <img class="portfolio-li-cont-img" src="../../../assets/images/projects-hover-1.png" alt="" srcset="">
+            <div class="p-li-title">Nomad Capital</div>
+            <div class="p-li-txt">Description text instance, up to two lines</div>
+            <div class="p-li-con">
+              <img src="../../../assets/images/svg/dark-svg1.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/dark-svg2.svg" alt="" srcset="">
+              <img src="../../../assets/images/svg/dark-svg3.svg" alt="" srcset="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang='ts'>
-import {defineComponent, reactive, toRefs, onMounted, getCurrentInstance } from 'vue'
-import {useRouter,useRoute} from 'vue-router'
+import { defineComponent, reactive, toRefs, onMounted, getCurrentInstance } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import headerBar from '../../../components/headerBar.vue'
 export default defineComponent({
-  components: {},
+  components: { headerBar },
   setup() {
     const { proxy } = getCurrentInstance() as any
     const router = useRouter()
@@ -30,4 +208,201 @@ export default defineComponent({
 })
 
 </script>
-<style scoped lang='scss'></style>
+<style scoped lang='scss'>
+.portfolio-con {
+  margin-top: 142px;
+
+  .con-title {
+    font-size: 48px;
+    color: #000;
+    font-weight: 600;
+    text-align: center;
+  }
+
+  .con-title1 {
+    font-size: 20px;
+    letter-spacing: 0.5px;
+    color: #666;
+    width: 830px;
+    text-align: center;
+    margin: 0 auto;
+    padding: 32px 0 100px 0;
+  }
+
+  .con-list {
+    max-width: 1000px;
+    margin: 0 auto;
+
+    .con-list-title {
+      display: flex;
+      align-items: center;
+      font-size: 20px;
+      color: #000;
+      font-weight: 600;
+
+      .con-title-line {
+        width: 798px;
+        height: 1px;
+        background: #999;
+        margin-left: 37px;
+      }
+    }
+
+    .portfolio-list {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      justify-content: space-between;
+      margin-top: 64px;
+
+      .portfolio-li {
+        width: 317px;
+        margin-bottom: 40px;
+
+        .portfolio-li-imgs {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 100%;
+          height: 211px;
+          border: 1px solid rgba(0, 0, 0, 0.10);
+          background: #000;
+          img {
+            height: 39px;
+            object-fit: cover;
+          }
+        }
+
+        .portfolio-li-con {
+          font-size: 16px;
+          color: #000;
+          margin-top: 24px;
+
+          .p-li-title {
+            color: #333;
+            font-weight: 600;
+          }
+
+          .p-li-txt {
+            width: 232px;
+            font-weight: 400;
+            margin-top: 12px;
+          }
+
+          .p-li-con {
+            display: flex;
+            align-items: center;
+            margin-top: 12px;
+
+            img {
+              width: 20px;
+              height: 20px;
+              margin-right: 16px;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  .con-listt {
+    max-width: 1000px;
+    margin: 0 auto;
+
+    .con-list-title {
+      display: flex;
+      align-items: center;
+      font-size: 20px;
+      color: #000;
+      font-weight: 600;
+
+      .con-title-line {
+        width: 790px;
+        height: 1px;
+        background: #999;
+        margin-left: 37px;
+      }
+    }
+
+    .portfolio-listt {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      justify-content: space-between;
+      margin-top: 64px;
+
+      .portfolio-li {
+        position: relative;
+        width: 25%;
+        cursor: pointer;
+
+        .portfolio-li-imgst {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 250px;
+          height: 250px;
+          border: 1px solid rgba(0, 0, 0, 0.10);
+          background: #FFF;
+
+          img {
+            height: 64px;
+          }
+        }
+
+        .portfolio-li-cont {
+          width: 100%;
+          font-size: 16px;
+          color: #fff;
+          position: absolute;
+          top: 1px;
+          left: 1px;
+          right: 1px;
+          bottom: 1px;
+          padding: 32px 16px 0px 16px;
+          background: #000;
+          box-sizing: border-box;
+
+          .portfolio-li-cont-img {
+            height: 20px;
+          }
+
+          .p-li-title {
+            font-weight: 500;
+            margin-top: 45px;
+          }
+
+          .p-li-txt {
+            font-size: 14px;
+            line-height: 20px;
+            width: 218px;
+            margin-top: 12px;
+          }
+
+          .p-li-con {
+            display: flex;
+            align-items: center;
+            margin-top: 20px;
+
+            img {
+              width: 20px;
+              height: 20px;
+              margin-right: 16px;
+            }
+          }
+
+          display: none;
+        }
+      }
+
+      .portfolio-li:hover {
+        .portfolio-li-cont {
+          display: block;
+        }
+      }
+    }
+
+  }
+
+
+}</style>
